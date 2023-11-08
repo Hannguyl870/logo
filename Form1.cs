6faa71b7fc3a6635f5a5271869a8b8cddbca9f3b
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-11-08 11:28:34 -0500

    ridy

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,34 @@ namespace logo
         {
             InitializeComponent();
         }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen redpen = new Pen(Color.Red, 10);
+            Pen Goldpen = new Pen(Color.Gold, 10);
+            Pen Navypen = new Pen(Color.Navy, 10);
+            Pen Whitepen = new Pen(Color.White, 10);
+            SolidBrush Goldbrush = new SolidBrush(Color.Gold);
+            SolidBrush Whitebrush = new SolidBrush(Color.White);
+
+            g.Clear(Color.White);
+
+            g.DrawArc(Navypen, 80, 20, 200, 200, 4, 270);
+
+            g.DrawEllipse(Goldpen, 100, 50, 150, 156);
+            g.FillEllipse(Goldbrush, 100, 50, 150, 156);
+
+            g.DrawRectangle(Whitepen, 100,100,150, 50);
+            g.FillRectangle(Whitebrush,100,100,150,50);
+
+            Font drawFont = new Font("flame", 20, FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+            g.DrawString("BURGER \n KING", drawFont, drawBrush, 100,100);
+
+
+        }
+
+       
     }
 }
